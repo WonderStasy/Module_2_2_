17438c3f0943f62689073ed4b88fa633a313988a
WonderStasy <stanislavflowers@ya.ru>
2024-09-03 22:09:43 +0300

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,10 @@
+first=int(input('число 1: '))
+second=int(input('число 2: '))
+third= int(input('число 3: '))
+
+if first == second == third:
+    print('3')
+elif first == second or first == third or second==third:
+    print('2')
+else:
+    print('0')
